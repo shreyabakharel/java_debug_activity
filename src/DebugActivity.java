6f09f9f1c123a8dfb9544a0c63be710764e4a339
shreyab <shreya.bakharel@gmail.com>
2023-06-13 13:04:32 -0700

    finished activity

--- src/DebugActivity.java
+++ src/DebugActivity.java
@@ -6,30 +6,35 @@ public class DebugActivity {
 
     public static void main(String[] args) {
         int x = 5;
-        int y = 0;
+        int y = 1;
 
         System.out.println("Starting the program...");
-
+        try {
+            int result = divideNumbers(x, y);
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Division by zero.");
+        }
 //            figure out how to "catch" the "divide by zero" error, don't just change the value on "y"
-        int result = divideNumbers(x, y)
+        int result = divideNumbers(x, y);
         System.out.println("Result: " + result);
 
-        String name = "John"
-        int length = name.length;
+        String name = "John";
+        int length = name.length();
         System.out.println("Length of name: " + length);
 
-        int numbers = {1, 2, 3, 4, 5};
-        for (int i = 0; i <= numbers.length; i++) {
+        int[] numbers = new int[]{1, 2, 3, 4, 5};
+        for (int i = 0; i < numbers.length; i++) {
 //                for loop should print one number each iteration
-            System.out.println(numbers);
+            System.out.println(numbers[i]);
         }
 
-        String[] fruits = ["Apple", "Banana", "Orange"];
-        for (String fruit : fruit) {
-            System.out.println(fruit)
+        String[] fruits = {"Apple", "Banana", "Orange"};
+        for (String fruit : fruits) {
+            System.out.println(fruit);
         }
 
-        System.out.println('End of the program.');
+        System.out.println("End of the program.");
     }
 
 }
